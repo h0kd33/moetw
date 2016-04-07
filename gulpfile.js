94a455b001b46626292470339ce09cb0f1538f3f
touhonoob <touhonoob@gmail.com>
2016-04-08 01:12:18 +0800

    add moe.html bundle

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var polybuild = require('polybuild');
 
 gulp.task('build', function() {
-    return gulp.src('./moe-video/moe-video.html')
-        .pipe(polybuild())
+    return gulp.src('./moe.html')
+        .pipe(polybuild({maximumCrush: true}))
         .pipe(gulp.dest('./dist'))
 });
